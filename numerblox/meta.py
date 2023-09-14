e0a35679f4912bdb1acf8cb3747c323c46d107b5
Carlo Lepelaars <carlo@crowdcent.com>
2023-09-14 11:09:59 +0000

    NumeraiEnsemble

--- numerblox/meta.py
+++ numerblox/meta.py
@@ -3,68 +3,103 @@
 import scipy
 import numpy as np
 import pandas as pd
-from typing import Union
+from typing import Union, List
 from sklearn.ensemble import VotingRegressor
 from sklearn.utils.validation import check_is_fitted
 
 
-# TODO Add groupby for multiple eras in standardization.
 class NumeraiEnsemble(VotingRegressor):
-    def __init__(self, estimators, weights=None, n_jobs=None, gaussianize=True, verbose=False):
+    """
+    Ensembler that standardizes predictions by era and averages them.
+    :param estimators: List of (name, estimator) tuples.
+    :param weights: Sequence of weights (float or int), optional, default: None.
+    If None, then uniform weights are used.
+    :param n_jobs: The number of jobs to run in parallel for fit.
+    Will revert to 1 CPU core if not defined.
+    -1 means using all processors.
+    :param gaussianize: Whether to gaussianize predictions before standardizing.
+    :param donate_weighted: Whether to use Donate et al.'s weighted average formula.
+    Often used when ensembling predictions from multiple folds over time.
+    Paper Link: https://doi.org/10.1016/j.neucom.2012.02.053
+    Example donate weighting for 5 folds: [0.0625, 0.0625, 0.125, 0.25, 0.5]
+    :param verbose: Whether to print info about ensembling.
+    """
+    def __init__(self, estimators, weights=None, n_jobs=None, gaussianize=True, 
+                 donate_weighted=False, verbose=False):
         super().__init__(estimators=estimators, weights=weights, n_jobs=n_jobs, verbose=verbose)
         self.gaussianize = gaussianize
+        self.donate_weighted = donate_weighted
+        # Override weights if donate_weighted is True
+        if self.donate_weighted:
+            weights = self._get_donate_weights()
+        else:
+            weights = self._weights_not_none
+        self.weights = weights
 
-    def predict(self, X: Union[np.array, pd.DataFrame]) -> np.array:
-        """ Normalize and average """
+    def predict(self, X: Union[np.array, pd.DataFrame], eras: np.array) -> np.array:
+        """ 
+        Standardize by era and average. 
+        :param X: Input data.
+        :param eras: Era labels (strings) for each row in X.
+        :return: Ensembled predictions.
+        """
+        assert len(X) == len(eras), "X and eras must have the same length."
         check_is_fitted(self)
-        # Get raw predictions
+        # Get raw predictions for each estimator
         pred_list = [est.predict(X) for est in self.estimators_]
-        # Standardize each array
-        standardized_pred_list = [self.standardize(x) for x in pred_list]
-        # Combine
+        # Standardization by era
+        standardized_pred_list = []
+        for pred in pred_list:
+            # Skip standardization if all predictions are the same
+            if np.all(pred == pred[0]):
+                print("Warning: Some estimator predictions are constant. Consider checking your estimators. Skipping these estimator predictions in ensembling.")
+            else:
+                standardized_pred = self.standardize_by_era(pred, eras)
+                standardized_pred_list.append(standardized_pred)
+
+        if len(standardized_pred_list) == 0:
+            raise ValueError("Predictions for all estimators are constant. No valid predictions to ensemble.")
         standardized_pred_arr = np.asarray(standardized_pred_list).T
-        # Average
-        return np.average(standardized_pred_arr, axis=1, weights=self._weights_not_none)
+
+        # Average out predictions
+        ensembled_predictions = np.average(standardized_pred_arr, axis=1, weights=self.weights)
+
+        # Raise error if values outside the [0...1] range.
+        if not np.all((0 <= ensembled_predictions) & (ensembled_predictions <= 1)):
+            raise ValueError("Ensembled predictions are not between 0 and 1. Consider checking your estimators.")
+        
+        return ensembled_predictions
 
     def standardize(self, X: np.array) -> np.array:
-        X = (scipy.stats.rankdata(X, method="ordinal") - 0.5) / len(X)
+        """ 
+        Standardize single era.
+        :param X: Predictions for a single era.
+        :return: Standardized predictions.
+        """
+        percentile_X = (scipy.stats.rankdata(X, method="ordinal") - 0.5) / len(X)
         if self.gaussianize:
-            X = scipy.stats.norm.ppf(X)
-        return X
-
-
-
-# class DonateWeightedEnsembler(BasePostProcessor):
-#     """
-#     Weighted average as per Donate et al.'s formula
-#     Paper Link: https://doi.org/10.1016/j.neucom.2012.02.053
-#     Code source: https://www.kaggle.com/gogo827jz/jane-street-supervised-autoencoder-mlp
-
-#     Weightings for 5 folds: [0.0625, 0.0625, 0.125, 0.25, 0.5]
-
-#     :param cols: Prediction columns to ensemble.
-#     Uses all prediction columns by default. \n
-#     :param final_col_name: New column name for ensembled values.
-#     :param verbose: Whether to print info about ensembling.
-#     """
-#     def __init__(self, final_col_name: str, cols: list = None, verbose: bool = True):
-#         super().__init__(final_col_name=final_col_name, verbose=verbose)
-#         self.cols = cols
-#         self.n_cols = len(cols)
-#         self.weights = self._get_weights()
-
-#     def transform(self, X: NumerFrame, y=None) -> NumerFrame:
-#         cols = self.cols if self.cols else X.prediction_cols
-#         X.loc[:, self.final_col_name] = np.average(
-#             X.loc[:, cols], weights=self.weights, axis=1
-#         )
-#         self._verbose_print_ensemble(self.cols)
-#         return NumerFrame(X)
-
-#     def _get_weights(self) -> list:
-#         """Exponential weights."""
-#         weights = []
-#         for j in range(1, self.n_cols + 1):
-#             j = 2 if j == 1 else j
-#             weights.append(1 / (2 ** (self.n_cols + 1 - j)))
-#         return weights
+            percentile_X = scipy.stats.norm.ppf(percentile_X)
+        return percentile_X
+    
+    def standardize_by_era(self, X: np.array, eras: np.array) -> np.array:
+        """
+        Standardize predictions of a single estimator by era.
+        :param X: All predictions of a single estimator.
+        :param eras: Era labels (strings) for each row in X.
+        :return: Standardized predictions.
+        """
+        df = pd.DataFrame({'prediction': X, 'era': eras})
+        df['standardized_prediction'] = df.groupby('era')['prediction'].transform(self.standardize)
+        return df['standardized_prediction'].values
+    
+    def _get_donate_weights(self) -> list:
+        """
+        Exponential weights as per Donate et al.'s formula.
+        Example donate weighting for 3 folds: [0.25, 0.25, 0.5]
+        Example donate weighting for 5 folds: [0.0625, 0.0625, 0.125, 0.25, 0.5]
+        """
+        weights = []
+        for j in range(1, len(self.estimators) + 1):
+            j = 2 if j == 1 else j
+            weights.append(1 / (2 ** (len(self.estimators) + 1 - j)))
+        return weights
--- tests/test_meta.py
+++ tests/test_meta.py
@@ -6,6 +6,7 @@ from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression, Ridge
 from scipy.stats import rankdata, norm
 from sklearn.utils.validation import check_is_fitted
+from sklearn.base import BaseEstimator, RegressorMixin
 
 from numerblox.meta import NumeraiEnsemble
 
@@ -25,6 +26,22 @@ def test_fit(ensemble, sample_data):
     check_is_fitted(ensemble)
     assert hasattr(ensemble, "estimators_")
     assert hasattr(ensemble, "get_feature_names_out")
+
+def test_predict(ensemble, sample_data):
+    X, y = sample_data
+    ensemble.fit(X, y)
+    # Dummy eras
+    eras = []
+    for i in range(1, 11):
+        eras.extend(["{:04}".format(i)] * 10)
+
+    preds = ensemble.predict(X, eras)
+    # The output of predict should have the same shape as y
+    assert preds.shape == y.shape
+    # Output should be a numpy array with values between 0 and 1
+    assert isinstance(preds, np.ndarray)
+    assert preds.min() >= 0
+    assert preds.max() <= 1
 
 def test_standardize_without_gaussianize(ensemble, sample_data):
     X, y = sample_data
@@ -50,11 +67,110 @@ def test_standardize_with_gaussianize(ensemble, sample_data):
     
     assert np.allclose(standardized_data, expected)
 
-def test_predict(ensemble, sample_data):
-    X, y = sample_data
-    ensemble.fit(X, y)
+def test_standardize_by_era():
+    ensemble = NumeraiEnsemble(estimators=None, gaussianize=False)
+
+    # Test 1: Basic functionality
+    X = np.array([0.5, 0.7, 0.1, 0.9, 0.6, 0.3])
+    eras = np.array([1, 1, 1, 2, 2, 2])
+    standardized = ensemble.standardize_by_era(X, eras)
+    # These values are simply computed based on manual calculations for rank and normalization
+    expected_values_1 = [0.5, 0.83333333, 0.16666667, 0.83333333, 0.5, 0.16666667]
+    assert np.allclose(standardized, expected_values_1)
+
+    # Test 2: Check standardized values for all same predictions split across two different eras
+    X = np.array([0.5, 0.5, 0.5, 0.5])
+    eras = np.array([1, 1, 2, 2])
+    standardized = ensemble.standardize_by_era(X, eras)
+    expected_values_2 = [0.25, 0.75, 0.25, 0.75]
+    assert np.allclose(standardized, expected_values_2)
+
+    # Test 3: Different predictions but split across two eras
+    X = np.array([0.1, 0.9, 0.9, 0.1])
+    eras = np.array([1, 1, 2, 2])
+    standardized = ensemble.standardize_by_era(X, eras)
+    expected_values_3 = [0.25, 0.75, 0.75, 0.25]
+    assert np.allclose(standardized, expected_values_3)
+
+
+class MockEstimator(BaseEstimator, RegressorMixin):
+    """ A mock estimator that always predicts a constant value. """
+    def fit(self, X, y):
+        pass
     
-    preds = ensemble.predict(X)
+    def predict(self, X):
+        return np.ones(X.shape[0]) * 3
+
+def test_predict_with_constant_values():
+    # Create an instance of your ensemble with mock estimators
+    estimators = [('mock1', MockEstimator()), ('mock2', MockEstimator())]
+    ensemble = NumeraiEnsemble(estimators)
     
-    # The output of predict should have the same shape as y
-    assert preds.shape == y.shape
+    X = np.random.rand(10, 3)
+    eras = np.random.randint(1, 5, size=10)
+    
+    X_fit = np.random.rand(10, 3)
+    y_fit = np.random.rand(10)
+    ensemble.fit(X_fit, y_fit)
+
+    with pytest.raises(ValueError, match="Predictions for all estimators are constant. No valid predictions to ensemble."):
+        ensemble.predict(X, eras)
+
+class ValidMockEstimator(BaseEstimator, RegressorMixin):
+    """A mock estimator that always predicts values within [0, 1] range."""
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        return np.random.uniform(size=len(X))
+
+class OutOfBoundsMockEstimator(BaseEstimator, RegressorMixin):
+    """A mock estimator that always predicts values out of [0, 1] range."""
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        return np.array([1.5] * len(X))
+
+def test_ensembled_predictions_out_of_bounds():
+    # Use the mock estimator for the ensemble
+    estimators = [('mock1', OutOfBoundsMockEstimator()), ('mock2', ValidMockEstimator())]
+    ensemble = NumeraiEnsemble(estimators)
+    
+    # Fit the ensemble with dummy data
+    X_fit = np.random.rand(10, 3)
+    y_fit = np.random.rand(10)
+    ensemble.fit(X_fit, y_fit)
+    
+    X = np.random.rand(10, 3)
+    eras = np.random.randint(1, 5, size=10)
+    
+    # The mock estimator will predict values of 1.5, which are out of [0, 1] range
+    with pytest.raises(ValueError, match="Ensembled predictions are not between 0 and 1. Consider checking your estimators."):
+        ensemble.predict(X, eras)
+
+def test_donate_weights():
+    estimators = [('mock1', ValidMockEstimator()), ('mock2', ValidMockEstimator()),
+                  ('mock2', ValidMockEstimator())]
+    ensemble = NumeraiEnsemble(estimators, donate_weighted=True)
+
+    # For 3 mock estimators, weights should be [0.25, 0.25, 0.5]
+    assert ensemble.weights == [0.25, 0.25, 0.5]
+
+    # For 5 mock estimators, weights should be [0.0625, 0.0625, 0.125, 0.25, 0.5]
+    estimators = [('mock1', ValidMockEstimator()), ('mock2', ValidMockEstimator()),
+                  ('mock3', ValidMockEstimator()), ('mock4', ValidMockEstimator()),
+                  ('mock5', ValidMockEstimator())]
+    ensemble = NumeraiEnsemble(estimators, donate_weighted=True)
+    assert ensemble.weights == [0.0625, 0.0625, 0.125, 0.25, 0.5]
+
+def test_donate_weights_sum_to_one():
+    for n_estimators in range(1, 11):
+        estimators = [('mock' + str(i), ValidMockEstimator()) for i in range(n_estimators)]
+        ensemble = NumeraiEnsemble(estimators, donate_weighted=True)
+
+        # Assert that the sum of weights is close to 1
+        assert np.isclose(sum(ensemble.weights), 1.0)
+
+
+                   
