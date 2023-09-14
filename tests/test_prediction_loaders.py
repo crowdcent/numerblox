4998476a9ed2244a2a945697e6836687a8509866
Carlo Lepelaars <carlo@crowdcent.com>
2023-09-14 10:04:13 +0000

    Prediction loaders + tests

---
+++ tests/test_prediction_loaders.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_regression
+
+
+from numerblox.prediction_loaders import ExamplePredictions
+
+
+def test_example_predictions_basic():
+    ep = ExamplePredictions()
+    preds = ep.fit_transform(None)
+    # Check all values are between 0 and 1
+    assert preds['prediction'].min() >= 0
+    assert preds['prediction'].max() <= 1
+    assert isinstance(preds, pd.DataFrame)
+
+def test_example_predictions_pipeline():
+    # Create dummy dataset
+    X, y = make_regression(n_samples=100, n_features=20, noise=0.1)
+    X = pd.DataFrame(X)
+    
+    # Create pipeline with standard scaler and example predictions
+    pipeline = Pipeline([
+        ('scaler', StandardScaler()),
+        ('predictions', ExamplePredictions())
+    ])
+    # Get results
+    preds = pipeline.fit_transform(X, y)
+
+    # Check all values are between 0 and 1
+    assert preds['prediction'].min() >= 0
+    assert preds['prediction'].max() <= 1
+    assert isinstance(preds, pd.DataFrame)
+
+def test_example_predictions_feature_union():
+    # Get predictions in basic setting to compare output
+    ep = ExamplePredictions()
+    preds = ep.fit_transform(None)
+
+    # Dummy data
+    X, _ = make_regression(n_samples=len(preds), n_features=2, noise=0.1)
+
+    # Create feature union
+    combined_features = FeatureUnion([
+        ('standard', StandardScaler()),
+        ('example', ExamplePredictions())
+    ])
+
+    # Transform data
+    X_transformed = combined_features.fit_transform(X)
+
+    # Ensure the transformation worked
+    assert np.allclose(X_transformed[:, -1], preds['prediction'].values)
+    assert X_transformed.shape[0] == X.shape[0]
+    assert X_transformed.shape[1] == 3
+
+
