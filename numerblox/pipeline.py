40deb045cd88b7bd63c942e2f98eef8a217d8ec2
Carlo Lepelaars <carlo@crowdcent.com>
2023-09-18 15:11:29 +0000

    NumeraiPipeline passes features and eras to preprocessors.

--- numerblox/pipeline.py
+++ numerblox/pipeline.py
@@ -20,7 +20,7 @@ class NumeraiPipeline(Pipeline):
             steps[-2] = (steps[-2][0], MetaEstimator(steps[-2][1]))
 
         # Make sure the last step requires features and eras arguments
-        if not self._has_required_args(steps[-1][1], "features", "eras"):
+        if not self._has_required_args(steps[-1][1], "features", "eras", type="estimator"):
             warnings.warn(f"""NumeraiPipeline is mostly used for use cases where the last step in the pipeline requires the arguments `'features'` and `'eras'`. For example, `numerblox.neutralizers.FeatureNeutralizer`. Got '{steps[-1][1].__class__.__name__}'. Be sure to pass the right arguments into the .predict method and consider if a regular `sklearn.pipeline.Pipeline` also works.""")
 
         self.steps = steps
@@ -29,12 +29,31 @@ class NumeraiPipeline(Pipeline):
 
     def predict(self, X, **params):
         """Custom predict to handle additional arguments."""
-        
         Xt = X
         for _, transform in self.steps[:-1]:
-            Xt = transform.transform(Xt)
-        
-        # Explicitly pass `features` and `eras` to the last step
+            # Define the method to be used
+            method = None
+            if hasattr(transform, "predict"):
+                method = transform.predict
+            elif hasattr(transform, "transform"):
+                method = transform.transform
+            else:
+                raise ValueError(f"{transform.__class__.__name__} has neither a 'predict' nor a 'transform' method.")
+            
+            # Check for required arguments
+            if self._has_required_args(transform, "eras", type="transformer"):
+                if "eras" not in params:
+                    raise ValueError(f"Argument 'eras' is required for {transform.__class__.__name__}.")
+                Xt = method(Xt, eras=params["eras"])
+            else:
+                Xt = method(Xt)
+
+        # Check if features and eras are passed for final step.
+        if "features" not in params:
+            raise ValueError(f"Argument 'features' is required for {self.steps[-1][0]}.")
+        if "eras" not in params:
+            raise ValueError(f"Argument 'eras' is required for {self.steps[-1][0]}.")
+        # Execute last step
         return self.steps[-1][-1].predict(Xt, **params)
     
     def transform(self, X, **params):
@@ -42,8 +61,15 @@ class NumeraiPipeline(Pipeline):
         return self.predict(X, **params)
     
     @staticmethod
-    def _has_required_args(transformer, *args):
-        sig = inspect.signature(transformer.predict)
+    def _has_required_args(transformer, *args, type="estimator"):
+        """ Check if transformer has required arguments. """
+        if type == "estimator":
+            method = transformer.predict
+        elif type == "transformer":
+            method = transformer.transform
+        else:
+            raise ValueError("type must be one of 'estimator' or 'transformer'.")
+        sig = inspect.signature(method)
         params = sig.parameters
         return all(arg in params for arg in args)
     
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -11,6 +11,28 @@ from utils import create_classic_sample_data
 
 setup_data = create_classic_sample_data
 
+class MockTransform:
+    """A mock transformer that requires 'eras' as an argument in its transform method."""
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, eras):
+        return X
+    
+    def predict(self, X, eras):
+        return self.transform(X, eras)
+    
+    def get_params(self, deep=True):
+        return {}  # or return actual parameters if any
+
+class MockFinalStep:
+    """A mock final step for the pipeline that requires 'features' and 'eras' in its predict method."""
+    def fit(self, X, y=None):
+        return self
+
+    def predict(self, X, features, eras):
+        return X
+
 def test_feature_neutralizer_pipeline(setup_data):
     lr1 = Ridge()
     fn = FeatureNeutralizer(proportion=0.5)
@@ -30,7 +52,42 @@ def test_numerai_pipeline_warning():
     steps = [("ridge", Ridge())]  
     
     with pytest.warns(UserWarning, match=r".*NumeraiPipeline is mostly used for.*"):
-        pipeline = NumeraiPipeline(steps)
+        _ = NumeraiPipeline(steps)
+
+def test_numerai_pipeline_missing_eras(setup_data):
+    # Create a pipeline where a step requires the 'eras' argument.
+    steps = [("mock_transform", MockTransform()), ("final_step", MockFinalStep())]
+    pipeline = NumeraiPipeline(steps)
+
+    X = setup_data[["feature1", "feature2"]]
+    y = setup_data["target"]
+    eras = setup_data["era"]
+
+    # Predict without providing 'eras' should raise a TypeError from MetaEstimator.
+    with pytest.raises(TypeError):
+        pipeline.fit(X, y).predict(X, features=[])
+
+def test_numerai_pipeline_missing_features(setup_data):
+    # Create a pipeline with a final step that requires 'features' and 'eras' arguments.
+    steps = [("ridge", Ridge()), ("final_step", MockFinalStep())]
+    pipeline = NumeraiPipeline(steps)
+
+    X = setup_data[["feature1", "feature2"]]
+    y = setup_data["target"]
+    # Predict without providing 'features' should raise an error.
+    with pytest.raises(ValueError, match=r"Argument 'features' is required for*."):
+        pipeline.fit(X, y).predict(X, eras=[])
+
+def test_numerai_pipeline_missing_eras_for_final_step(setup_data):
+    # Create a pipeline with a final step that requires 'features' and 'eras' arguments.
+    steps = [("ridge", Ridge()), ("final_step", MockFinalStep())]
+    pipeline = NumeraiPipeline(steps)
+
+    X = setup_data[["feature1", "feature2"]]
+    y = setup_data["target"]
+    # Predict without providing 'eras' for the final step should raise an error.
+    with pytest.raises(ValueError, match=r"Argument 'eras' is required for*."):
+        pipeline.fit(X, y).predict(X, features=[])
 
 def test_feature_neutralizer_featureunion(setup_data):
     onehot = OneHotEncoder(sparse_output=False)
