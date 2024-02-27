b05b4a784a7dd07eb6be60c23fa91b1197d5c2c1
Carlo Lepelaars <info@carlolepelaars.nl>
2024-02-27 20:18:59 +0100

    NumeraiSignalsDownloader. Update numerapi and numerai-tools. Docs. Version 1.2

--- tests/test_download.py
+++ tests/test_download.py
@@ -4,16 +4,18 @@ import pandas as pd
 from uuid import uuid4
 from pathlib import PosixPath
 
-from numerapi import NumerAPI
-from numerblox.download import NumeraiClassicDownloader, KaggleDownloader, EODDownloader
+from numerapi import NumerAPI, SignalsAPI
+from numerblox.download import NumeraiClassicDownloader, NumeraiSignalsDownloader, KaggleDownloader, EODDownloader
 
-ALL_DATASET_VERSIONS = set(s.split("/")[0] for s in NumerAPI().list_datasets())
-ALL_DATASET_VERSIONS.discard("signals")
-TEST_DIR = f"test_numclassic_general_{uuid4()}"
-TEST_VERSIONS = ["4.2", "4.3"]
+ALL_CLASSIC_VERSIONS = set(s.split("/")[0] for s in NumerAPI().list_datasets() if not "signals" in s)
+ALL_SIGNALS_VERSIONS = set(s.replace("signals/", "").split("/")[0] for s in SignalsAPI().list_datasets() if s.startswith("signals/v"))
+TEST_CLASSIC_DIR = f"test_numclassic_general_{uuid4()}"
+TEST_SIGNALS_DIR = f"test_numsignals_general_{uuid4()}"
+TEST_CLASSIC_VERSIONS = ["4.2", "4.3"]
+TEST_SIGNALS_VERSIONS = ["1.0"]
 
 def test_base():
-    numer_classic_downloader = NumeraiClassicDownloader(TEST_DIR)
+    numer_classic_downloader = NumeraiClassicDownloader(TEST_CLASSIC_DIR)
 
     # Test building class
     assert isinstance(numer_classic_downloader.dir, PosixPath)
@@ -25,19 +27,20 @@ def test_base():
 
     # Remove contents
     numer_classic_downloader.remove_base_directory()
-    assert not os.path.exists(TEST_DIR)
+    assert not os.path.exists(TEST_CLASSIC_DIR)
+
 
 def test_classic():
-    dl = NumeraiClassicDownloader(TEST_DIR)
+    dl = NumeraiClassicDownloader(TEST_CLASSIC_DIR)
 
     # Check versions
-    assert dl.dataset_versions == ALL_DATASET_VERSIONS
+    assert dl.dataset_versions == ALL_CLASSIC_VERSIONS
 
-    # Test inference download
-    for version in TEST_VERSIONS:
-        dl.download_inference_data("inference", version=version)
-        assert os.path.exists(dl.dir / "inference")
-        assert os.path.exists(dl.dir / "inference" / "live_int8.parquet")
+    # Test live download
+    for version in TEST_CLASSIC_VERSIONS:
+        dl.download_live_data("live", version=version)
+        assert os.path.exists(dl.dir / "live")
+        assert os.path.exists(dl.dir / "live" / "live_int8.parquet")
 
         # Test example data
         dl.download_example_data("test/", version=version)
@@ -61,6 +64,28 @@ def test_classic():
 
     dl.remove_base_directory()
 
+
+def test_signals():
+    dl = NumeraiSignalsDownloader(TEST_SIGNALS_DIR)
+
+    # Check versions
+    assert dl.dataset_versions == ALL_SIGNALS_VERSIONS
+
+    # Test live download
+    for version in TEST_SIGNALS_VERSIONS:
+        dl.download_live_data("live", version=version)
+        assert os.path.exists(dl.dir / "live")
+        assert os.path.exists(dl.dir / "live" / "live.parquet")
+
+        # Test example data
+        dl.download_example_data("test/", version=version)
+        assert os.path.exists(dl.dir / "test")
+        assert os.path.exists(dl.dir / "test" / "live_example_preds.parquet")
+        assert os.path.exists(dl.dir / "test" / "validation_example_preds.parquet")
+
+    dl.remove_base_directory()
+
+
 def test_classic_versions():
     downloader = NumeraiClassicDownloader(directory_path=f"some_path_{uuid4()}")
 
@@ -70,11 +95,24 @@ def test_classic_versions():
         with pytest.raises(AssertionError):
             downloader.download_training_data(version=version)
         with pytest.raises(AssertionError):
-            downloader.download_inference_data(version=version)
+            downloader.download_live_data(version=version)
+            
+    downloader.remove_base_directory()
+
+
+def test_signals_versions():
+    downloader = NumeraiSignalsDownloader(directory_path=f"some_path_{uuid4()}")
+
+    # Test unsupported versions
+    unsupported_versions = ["0", "0.5", "2", "3.5"]
+    for version in unsupported_versions:
+        with pytest.raises(AssertionError):
+            downloader.download_training_data(version=version)
         with pytest.raises(AssertionError):
             downloader.download_live_data(version=version)
             
     downloader.remove_base_directory()
+
 
 def test_kaggle_downloader():
     try:
@@ -84,9 +122,10 @@ def test_kaggle_downloader():
     except OSError:
         pass
 
+
 def test_eod():
     eod = EODDownloader(f"test_eod_{uuid4()}", key="DEMO", tickers=["AAPL.US"])
-    eod.download_inference_data()
+    eod.download_live_data()
     eod.download_training_data()
     eod.remove_base_directory()
     
