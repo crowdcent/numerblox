204b57372702f555bb9188e72cc427830066e297
Carlo Lepelaars <info@carlolepelaars.nl>
2024-08-28 20:44:56 +0200

    rcond=None for lstsq raw exposure calculation

--- numerblox/neutralizers.py
+++ numerblox/neutralizers.py
@@ -196,5 +196,5 @@ class FeatureNeutralizer(BaseNeutralizer):
         :param scores: DataFrame with predictions.
         :return: Raw exposures for each era.
         """
-        return exposures.dot(np.linalg.lstsq(exposures, scores))[0]
+        return exposures.dot(np.linalg.lstsq(exposures, scores, rcond=None))[0]
     
