610fd95ce9b2bcedaabe7e095c3a2fec92b9814f
Carlo Lepelaars <info@carlolepelaars.nl>
2024-09-02 20:54:48 +0200

    NumeraiCryptoDownloader. Refactor downloader tests. Relax main dependency versions

---
+++ tests/test_download/test_download_crypto.py
@@ -0,0 +1,36 @@
+import os
+import pytest
+from uuid import uuid4
+
+from numerblox.download import NumeraiCryptoDownloader
+
+
+ALL_CRYPTO_VERSIONS = ["v1.0"]
+
+def test_crypto():
+    TEST_CRYPTO_DIR = f"test_numcrypto_general_{uuid4()}"
+    dl = NumeraiCryptoDownloader(TEST_CRYPTO_DIR)
+
+    # Check versions
+    assert dl.dataset_versions == ALL_CRYPTO_VERSIONS
+
+    # Test live download
+    dl.download_live_data("live", version="1.0")
+    assert os.path.exists(dl.dir / "live")
+    assert os.path.exists(dl.dir / "live" / "live_universe.parquet")
+
+    # Test training data download
+    dl.download_training_data("train/", version="1.0")
+    assert os.path.exists(dl.dir / "train")
+    assert os.path.exists(dl.dir / "train" / "train_targets.parquet")
+
+def test_crypto_versions():
+    downloader = NumeraiCryptoDownloader(directory_path=f"some_path_{uuid4()}")
+
+    # Test unsupported versions
+    unsupported_versions = ["0", "0.5", "3.5"]
+    for version in unsupported_versions:
+        with pytest.raises(AssertionError):
+            downloader.download_training_data(version=version)
+        with pytest.raises(AssertionError):
+            downloader.download_live_data(version=version)
---
+++ tests/test_download/test_download_signals.py
@@ -0,0 +1,58 @@
+import os
+import pytest
+from uuid import uuid4
+from numerapi import SignalsAPI
+
+from numerblox.download import NumeraiSignalsDownloader, KaggleDownloader, EODDownloader
+
+ALL_SIGNALS_VERSIONS = set(s.replace("signals/", "").split("/")[0] for s in SignalsAPI().list_datasets() if s.startswith("signals/v"))
+TEST_SIGNALS_DIR = f"test_numsignals_general_{uuid4()}"
+TEST_SIGNALS_VERSIONS = ["1.0"]
+
+
+def test_signals():
+    dl = NumeraiSignalsDownloader(TEST_SIGNALS_DIR)
+
+    # Check versions
+    assert dl.dataset_versions == ALL_SIGNALS_VERSIONS
+
+    # Test live download
+    for version in TEST_SIGNALS_VERSIONS:
+        dl.download_live_data("live", version=version)
+        assert os.path.exists(dl.dir / "live")
+        assert os.path.exists(dl.dir / "live" / "live.parquet")
+
+        # Test example data
+        dl.download_example_data("test/", version=version)
+        assert os.path.exists(dl.dir / "test")
+        assert os.path.exists(dl.dir / "test" / "live_example_preds.parquet")
+        assert os.path.exists(dl.dir / "test" / "validation_example_preds.parquet")
+
+    dl.remove_base_directory()
+
+def test_signals_versions():
+    downloader = NumeraiSignalsDownloader(directory_path=f"some_path_{uuid4()}")
+
+    # Test unsupported versions
+    unsupported_versions = ["0", "0.5", "3.5"]
+    for version in unsupported_versions:
+        with pytest.raises(AssertionError):
+            downloader.download_training_data(version=version)
+        with pytest.raises(AssertionError):
+            downloader.download_live_data(version=version)
+            
+    downloader.remove_base_directory()
+
+def test_kaggle_downloader():
+    try:
+        kd = KaggleDownloader(f"test_kaggle_{uuid4()}")
+        assert os.path.exists(kd.dir)
+        kd.remove_base_directory()
+    except OSError:
+        pass
+
+def test_eod():
+    eod = EODDownloader(f"test_eod_{uuid4()}", key="DEMO", tickers=["AAPL.US"])
+    eod.download_live_data()
+    eod.download_training_data()
+    eod.remove_base_directory()
