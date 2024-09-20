c66ba25098ae705f6a415f308ce3145b1d94b1a6
Carlo Lepelaars <info@carlolepelaars.nl>
2024-09-20 15:56:45 +0200

    use new val data in test utils

--- tests/utils.py
+++ tests/utils.py
@@ -65,4 +65,4 @@ def create_signals_sample_data():
 
 @pytest.fixture()
 def classic_test_data():
-    return pd.read_parquet("tests/test_assets/train_int8_5_eras.parquet")
+    return pd.read_parquet("tests/test_assets/val_3_eras.parquet")
