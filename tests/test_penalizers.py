10c293d0734e647607ed1348725c5b5bd087ba5a
Carlo Lepelaars <info@carlolepelaars.nl>
2024-09-30 14:29:47 +0200

    Refine uv pyproject

--- tests/test_penalizers.py
+++ tests/test_penalizers.py
@@ -1,5 +1,5 @@
 import pytest
-from numerblox.penalizers import BasePenalizer, FeaturePenalizer, tf
+from numerblox.penalizers import BasePenalizer, FeaturePenalizer
 
 from utils import create_classic_sample_data
 
@@ -13,23 +13,23 @@ def test_base_penalizer_fit(setup_data):
     obj = BasePenalizer(new_col_name="test").fit(setup_data)
     assert isinstance(obj, BasePenalizer)
 
-@pytest.mark.xfail(tf is None, reason="TensorFlow is not installed")
+@pytest.mark.xfail(reason="TensorFlow is not installed")
 def test_feature_penalizer_initialization():
     fp = FeaturePenalizer(max_exposure=0.5)
     assert fp.new_col_name.startswith("prediction_penalized_")
     assert fp.max_exposure == 0.5
 
-@pytest.mark.xfail(tf is None, reason="TensorFlow is not installed")
+@pytest.mark.xfail(reason="TensorFlow is not installed")
 def test_feature_penalizer_get_feature_names_out():
     names = FeaturePenalizer(max_exposure=0.5).get_feature_names_out()
     assert names == ["prediction_penalized_0.5"]
 
-@pytest.mark.xfail(tf is None, reason="TensorFlow is not installed")
+@pytest.mark.xfail(reason="TensorFlow is not installed")
 def test_feature_penalizer_get_feature_names_out_complex():
     names = FeaturePenalizer(max_exposure=0.7, pred_name="fancy", suffix="blob").get_feature_names_out()
     assert names == ["fancy_penalized_0.7_blob"]
 
-@pytest.mark.xfail(tf is None, reason="TensorFlow is not installed")
+@pytest.mark.xfail(reason="TensorFlow is not installed")
 def test_feature_penalizer_get_feature_names_out_with_input_features():
     names = FeaturePenalizer(max_exposure=0.5).get_feature_names_out(input_features=["prediction_fancy1"])
     assert names == ["prediction_fancy1"]
